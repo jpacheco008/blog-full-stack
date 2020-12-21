a6753ba044700677774783880398365d50286095
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2020-12-21 16:14:44 -0500

    Heiarchy for client

---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css'
+import List from '../../components/List/List'
+
+const Home = () => {
+  return (
+    <div>
+      We are working
+      <List />
+    </div>
+  )
+}
+ 
+export default Home
