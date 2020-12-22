e0d01edabeefbd70794c4e6551cf9af84476aea7
jpacheco008 <69394703+jpacheco008@users.noreply.github.com>
2020-12-21 21:49:56 -0500

    changes to blog and list

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,9 +1,29 @@
 import './List.css'
+import { Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import './List.css'
+import Blog from '../Blog/Blog'
+import { getBlogs } from '../../services/blogs'
+import Nav from '../Nav/Nav'
 
 const List = () => {
+  const [allBlogs, setAllBlogs] = useState([])
+
+  useEffect(() => {
+    const fecthBlogs = async () => {
+      const blogs = await getBlogs()
+      setAllBlogs(blogs)
+    }
+    fecthBlogs()
+  }, [])
+
+  
   return (
     <div>
-      
+      <Nav />
+      {allBlogs.map((blog) => {
+        return <Blog id={blog.id} name={blog.name} imgURL={blog.imgURL} comment={blog.comment}/>
+      })}
     </div>
   )
 }
